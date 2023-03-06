0e6a86d3a292f18a3a715b84dc76302313e9f3e7
mazlumcakmak <101354204+mazlumcakmak@users.noreply.github.com>
2023-03-06 20:45:13 +0300

    Update customComboboxBuilder.js

--- customComboboxBuilder.js
+++ customComboboxBuilder.js
@@ -20,6 +20,26 @@
                 this.dispatchEvent(event);
             });
         }
+         async connectedCallback() {
+      this.initMain();
+    }
+    async initMain() {
+      const date = this._props.date;
+      const caption = this._props.captionaftercountdown;
+      const countdownEl = this.shadowRoot.querySelector("#countdown");
+      const divEl = this.shadowRoot.querySelector("#time_div");
+      const updateCountdown = () => {
+        const timeRemaining = Date.parse(date) - Date.parse(new Date());
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+        const seconds = Math.floor((timeRemaining / 1000) % 60);
+        countdownEl.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        if (timeRemaining < 0) {
+          countdownEl.innerHTML = caption;
+        }
+      };
         customElements.define("custom-button",
             customComboboxBuilderPanel);
     })();
