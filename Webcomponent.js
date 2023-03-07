39ce6aea86c4ad94f688ca5dac19928a4c000ecf
mazlumcakmak <101354204+mazlumcakmak@users.noreply.github.com>
2023-03-07 11:02:00 +0300

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -26,6 +26,11 @@
             xmlHttp.open( "GET", "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/FactData?deltaid=first-abad8f16-9ce9-4d1a-9b3e-39d5ca8cd4e5", false ); // false for synchronous request
             xmlHttp.send( null );
             console.log(xmlHttp.responseText);
+            var lt_parser =  JSON.parse(xmlHttp.responseText);
+            var lt_values = lt_parser.value;
+            for(var i = 0; i < lt_values.length; i++ ){
+                console.log(lt_values[i].PRICE);
+            }
             
         }        
         
