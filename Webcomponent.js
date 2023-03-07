54e36463014ffbac7f9748962d602f537a1db900
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-07 16:54:28 +0300

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -6,25 +6,31 @@
         constructor() {
             super();
             this.init();
-            //var xmlHttp = new XMLHttpRequest();
-            /*
-            xmlHttp.open("GET", "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/SAP_FI_IFP_GLACCOUNTMaster", false); // false for synchronous request
-            xmlHttp.send(null);
-            console.log(xmlHttp.responseText);
-            var lt_parser = JSON.parse(xmlHttp.responseText);
-            var lt_values = lt_parser.value;
-            debugger;
-            var select = this.shadowRoot.getElementById("dimDropDownSel");
-            for (var i = 0; i < lt_values.length; i++) {
-            console.log("=====================");
-            console.log("ID:", lt_values[i].ID);
-            console.log("Description:", lt_values[i].Description);
-            var option = document.createElement("OPTION");
-            option.innerHTML = lt_values[i].Description;
-            option.value = lt_values[i].ID;
-            select.options.add(option);
+            this._props = {};
+			debugger;
+            const dimension = this._props.dimension;
+            const dimensionType = this._props.dimensionType;
+            if (dimension != "") {
+                var xmlHttp = new XMLHttpRequest();
+				var lvUrl = "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/"+dimension+"Master";
+                xmlHttp.open("GET", lvUrl, false); // false for synchronous request
+                xmlHttp.send(null);
+                console.log(xmlHttp.responseText);
+                var lt_parser = JSON.parse(xmlHttp.responseText);
+                var lt_values = lt_parser.value;
+                debugger;
+                var select = this.shadowRoot.getElementById("dimDropDownSel");
+                for (var i = 0; i < lt_values.length; i++) {
+                    console.log("=====================");
+                    console.log("ID:", lt_values[i].ID);
+                    console.log("Description:", lt_values[i].Description);
+                    var option = document.createElement("OPTION");
+                    option.innerHTML = lt_values[i].Description;
+                    option.value = lt_values[i].ID;
+                    select.options.add(option);
+                }
             }
-             */
+
         }
 
         init() {
@@ -41,6 +47,12 @@
         }
 
         fireChanged() {}
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = {
+                ...this._props,
+                ...changedProperties
+            };
+        }
 
     }
 
