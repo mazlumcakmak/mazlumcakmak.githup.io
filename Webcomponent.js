2559090b3b17f81b3069c428d7085404465d9a34
mazlumcakmak <101354204+mazlumcakmak@users.noreply.github.com>
2023-03-07 10:48:27 +0300

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -22,6 +22,10 @@
 
         fireChanged() {
             console.log("OnClick Triggered");     
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.open( "GET", "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/FactData?deltaid=first-abad8f16-9ce9-4d1a-9b3e-39d5ca8cd4e5", false ); // false for synchronous request
+            xmlHttp.send( null );
+            console.log(xmlHttp.responseText);
             
         }        
         
