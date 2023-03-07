39edacab4e568f99668fbd9c6f5454d0d6f3ceaf
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-07 14:06:18 +0300

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,6 +1,6 @@
 (function () {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = '<select name="example"> <option value="A">A</option> <option value="B">B</option> <option value="-">Other</option> </select>';
+    tmpl.innerHTML = '<select name="dimDropDownSel"> <option value="A">A</option> <option value="B">B</option> <option value="-">Other</option> </select>';
 
     class PerformanceHelp extends HTMLElement {
         constructor() {
@@ -24,36 +24,20 @@
         fireChanged() {
             console.log("OnClick Triggered");
             var xmlHttp = new XMLHttpRequest();
-            xmlHttp.open("GET", "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/FactData?deltaid=first-abad8f16-9ce9-4d1a-9b3e-39d5ca8cd4e5", false); // false for synchronous request
+            xmlHttp.open("GET", "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/SAP_FI_IFP_GLACCOUNTMaster", false); // false for synchronous request
             xmlHttp.send(null);
             console.log(xmlHttp.responseText);
             var lt_parser = JSON.parse(xmlHttp.responseText);
             var lt_values = lt_parser.value;
             for (var i = 0; i < lt_values.length; i++) {
                 console.log("=====================");
-                console.log("Price:", lt_values[i].PRICE);
-                console.log("QUANTITY:", lt_values[i].QUANTITY);
-                console.log("COMPPRICE:", lt_values[i].COMPPRICE);
-                console.log("LOTAMOUNT:", lt_values[i].LOTAMOUNT);
-                console.log("SALESQUANTITY:", lt_values[i].SALESQUANTITY);
-                console.log("AMOUNT:", lt_values[i].AMOUNT);
-                console.log("COMPONENTQUANTITY:", lt_values[i].COMPONENTQUANTITY);
-                console.log("LOTSIZE:", lt_values[i].LOTSIZE);
-                console.log("SAP_FI_IFP_QUANTITY_UNIT:", lt_values[i].SAP_FI_IFP_QUANTITY_UNIT);
-                console.log("SAP_FI_IFP_GLACCOUNT:", lt_values[i].SAP_FI_IFP_GLACCOUNT);
-                console.log("Date:", lt_values[i].SAP_FI_IFP_GLACCOUNT);
-                console.log("SAP_FI_IFP_GLACCOUNT:", lt_values[i].Date);
-                console.log("SAP_FI_IFP_FIXVARIABLE:", lt_values[i].SAP_FI_IFP_FIXVARIABLE);
-                console.log("Version:", lt_values[i].Version);
-                console.log("SAP_FI_IFP_COMPONENTPLANT:", lt_values[i].SAP_FI_IFP_COMPONENTPLANT);
-                console.log("SAP_ALL_COSTCENTER:", lt_values[i].SAP_ALL_COSTCENTER);
-                console.log("SAP_FI_IFP_LOTSIZE_UNIT:", lt_values[i].SAP_FI_IFP_LOTSIZE_UNIT);
-                console.log("SAP_ALL_PLANT:", lt_values[i].SAP_ALL_PLANT);
-                console.log("SAP_ALL_MATERIAL:", lt_values[i].SAP_ALL_MATERIAL);
-                console.log("SAP_FI_IFP_CCACTIVITYTYPE:", lt_values[i].SAP_FI_IFP_CCACTIVITYTYPE);
-                console.log("SAP_ALL_PRODUCT:", lt_values[i].SAP_ALL_PRODUCT);
-                console.log("SAP_FI_IFP_SALESQUANTITY_UNIT:", lt_values[i].SAP_FI_IFP_SALESQUANTITY_UNIT);
-                console.log("=====================");
+                console.log("ID:", lt_values[i].ID);
+                console.log("Description:", lt_values[i].Description);  
+				var select = document.getElementById("dimDropDownSel");
+				var option = document.createElement('option');
+				option.text = lt_values[i].Description;
+				option.value  = lt_values[i].ID;
+				select.add(option, 0);
             }
 
         }
