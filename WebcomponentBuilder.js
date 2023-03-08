aea0b77edda197b0d0754e5031367d2c020365d1
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-08 23:25:26 +0300

    Update WebcomponentBuilder.js

--- WebcomponentBuilder.js
+++ WebcomponentBuilder.js
@@ -2,6 +2,7 @@
 (function () {
     let gv_provider = "";
     let gv_dimension = "";
+    let gv_completed = false;
     let template = document.createElement("template");
     template.innerHTML = '<br> <style> #form { font-family: Arial, sans-serif;width: 400px; margin: 0 audimensionType; } a { text-decoration: none; } #table { width: 100%; border-collapse: collapse; margin-botdimensionTypem: 10px;} #td { padding: 15px; text-align: left; font-size: 13px; } input { width: 100%; padding: 10px; border: 2px solid #ccc; border-radius: 5px; font-size: 13px; box-sizing: border-box; margin-botdimensionTypem: 10px;} #tdFilter { width: 80%; padding: 15px; text-align: left; font-size: 13px; } input[type="color"] { -webkit-appearance: none; border: none; width: 32px; height: 32px; } input[type="color"]::-webkit-color-swatch-wrapper {padding: 0; } input[type="color"]::-webkit-color-swatch {border: none; } input[type="submit"] { background-color: #487cac; color: white; padding: 10px; border: none; border-radius: 5px; font-size: 14px; cursor: pointer; width: 100%; } input[type="button"] { background-color: #487cac; color: white; padding: 10px; border: none; border-radius: 5px; font-size: 14px; cursor: pointer; width: 100%; } #label { width: 140px; } #tableFilter { font-family: arial, sans-serif;border-collapse: collapse; width: 100%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) { background-color: #dddddd; } #tr:nth-child(even) { background-color: #FFFFFF; } select { width: 100%; min-width: 15ch; max-width: 300ch; border: 1px solid; border-radius: 0.25em; padding: 0.25em 0.5em; font-size: 0.9rem; cursor: pointer; line-height: 1.1; background-color: #fff; background-image: linear-gradient(to top, #f9f9f9, #fff 33%);position: relative; border: 2px solid #ccc; border-radius: 5px; box-sizing: border-box; margin-botdimensionTypem: 10px;} </style> <form id="form"> <table id="table"> <tr id="tr"> <td id="td"> <p>Provider</p> <select id="builder_provider"><option value = "none"  >loading..</option></select> </td> </tr> <tr id="tr"> <td id="td"> <p>Dimension Type</p> <input id="builder_dimensionType" type="text" placeholder="Master Data / Transaction Data"></td> </tr> <tr id="tr"> <td id="td"> <p>Dimension Name</p> <select id="builder_dimension"><option value = "none" >loading..</option></select> </td> </tr> <tr id="tr"> <td id="td"> <p>Display Option</p> <select id="builder_displayOpt"><option>Description</option> <option>ID</option> <option>ID - Description</option><option>Description - ID</option></select> </td> </tr> </table> <table id="table"> <tr id="tr"> <td id="td"> <p>Filter Name</p> <input style="width: 70%;" id="filterName" class="filterName" type="text" placeholder="Filter Name"><input id="addBtn" style="width: 20%;" value="Add" type="button" onclick="myFunction()"></td> </tr> </table> <br> <br> <table id="tableFilter" class="tableFilter"><tr> <th>Filter Value</th> </tr> <tr> <td></td> </tr> </table> <br> <br> <br> <input value="Update Settings" type="submit"><br> </form>';
     class DimensionWidgetBuilderPanel extends HTMLElement {
@@ -20,7 +21,7 @@
             .getElementById("builder_provider")
             .addEventListener("change", this.fireChanged.bind(this));
             this.fillModel();
-            this.fillDimension();
+            //this.fillDimension();
 
         }
         _submit(e) {
@@ -103,11 +104,13 @@
 
         set dimension(_dimension) {
             debugger;
-            var lv_dimension = this._shadowRoot.getElementById("builder_dimension").value;
-            if (lv_dimension == "none" && gv_dimension == "") {
-                gv_dimension = _dimension;
-            } else {
-                this._shadowRoot.getElementById("builder_dimension").value = _dimension;
+            gv_dimension = _dimension;
+            if (gv_dimension != "" && gv_provider != "") {
+                this.fillModel();
+                this._shadowRoot.getElementById("builder_provider").value = gv_provider;
+                this.fillDimension();
+                this._shadowRoot.getElementById("builder_dimension").value = gv_dimension;
+
             }
 
         }
@@ -124,25 +127,12 @@
 
         set prodiver(_prodiver) {
             debugger;
-            //var e = this._shadowRoot.getElementById("builder_provider");
-            //return e.options[e.selectedIndex].value = _prodiver;
-			if(_prodiver == "" || _prodiver == undefined){
-				this.fillModel();
-				return;
-			}
-            var lv_f = false;
-            var lv_provider = this._shadowRoot.getElementById("builder_provider").value;
-            if (lv_provider == "none" && gv_provider == "") {
-                this.fillModel();
-                lv_f = true;
-            }
             gv_provider = _prodiver;
-            this._shadowRoot.getElementById("builder_provider").value = _prodiver;
-            if (lv_f) {
+            if (gv_dimension != "" && gv_provider != "") {
+                this.fillModel();
+                this._shadowRoot.getElementById("builder_provider").value = gv_provider;
                 this.fillDimension();
-                this.dimension = gv_dimension
-
-                    //this._shadowRoot.getElementById("builder_dimension").value = gv_dimension;
+                this._shadowRoot.getElementById("builder_dimension").value = gv_dimension;
 
             }
 
