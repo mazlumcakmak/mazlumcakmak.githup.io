55080a36198884ceb3c3351c4e47b9935feca7de
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-09 15:21:50 +0300

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -25,18 +25,30 @@
             });
         }
         getData() {
+            const displayOpt = this._props.displayOpt;
+            switch (displayOpt) {
+            case "Master":
+                this.getMasterData();
+                break;
+            case "Transaction":
+                this.getTransactionData();
+                break;
+            }
+        }
+
+        getMasterData() {
             const dimension = this._props.dimension;
-            const dimensionType = this._props.dimensionType; 
+            const dimensionType = this._props.dimensionType;
             const prodiver = this._props.prodiver;
             const displayOpt = this._props.displayOpt;
-			
-            if (dimension != "" && dimension != undefined &&  prodiver != "" && prodiver != undefined) {
-                var xmlHttp = new XMLHttpRequest(); 
-				var lvUrl = "https://" + window.location.host + "/api/v1/dataexport/providers/sac/"+ prodiver + "/" + dimension + "Master";
+
+            if (dimension != "" && dimension != undefined && prodiver != "" && prodiver != undefined) {
+                var xmlHttp = new XMLHttpRequest();
+                var lvUrl = "https://" + window.location.host + "/api/v1/dataexport/providers/sac/" + prodiver + "/" + dimension + "Master";
                 //"https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/" + dimension + "Master";
                 //https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C9Z996O1NC1N4P3AWYHVPEXP8G/FactData?$filter=contains(Version,%27public.Plan%27)
                 xmlHttp.open("GET", lvUrl, true); // false for synchronous request
-                xmlHttp.onload = (e) => { 
+                xmlHttp.onload = (e) => {
                     if (xmlHttp.readyState === 4) {
                         if (xmlHttp.status === 200) {
                             var lt_parser = JSON.parse(xmlHttp.responseText);
@@ -83,8 +95,91 @@
 
             }
         }
+        getTransactionData() {
+            const dimension = this._props.dimension;
+            const dimensionType = this._props.dimensionType;
+            const prodiver = this._props.prodiver;
+            const displayOpt = this._props.displayOpt;
+            if (dimension != "" && dimension != undefined && prodiver != "" && prodiver != undefined) {
+                var xmlHttpMaster = new XMLHttpRequest();
+                var lvUrl = "https://" + window.location.host + "/api/v1/dataexport/providers/sac/" + prodiver + "/" + dimension + "Master";
+                lvUrl = "https://" + window.location.host + "/api/v1/dataexport/providers/sac/" + prodiver + "/FactData";
+                // get member service
+                xmlHttpMaster.open("GET", lvUrl, true);
+                xmlHttpMaster.onload = (e) => {
+                    if (xmlHttpMaster.readyState === 4) {
+                        if (xmlHttpMaster.status === 200) {
+                            var lt_parser = JSON.parse(xmlHttpMaster.responseText);
+                            var lt_values = lt_parser.value;
+                            var lvUrlFact = "https://" + window.location.host + "/api/v1/dataexport/providers/sac/" + prodiver + "/FactData";
+                            var xmlHttpFact = new XMLHttpRequest();
+                            // get transaction data
+                            xmlHttpFact.open("GET", lvUrlFact, true);
+                            xmlHttpFact.onload = (e) => {
+                                if (xmlHttpFact.readyState === 4) {
+                                    if (xmlHttpFact.status === 200) {
+                                        var lt_parserFatc = JSON.parse(xmlHttpFact.responseText);
+                                        var lt_valuesFatc = lt_parserFatc.value;
+                                        this.shadowRoot.getElementById("dimDropDownSel").innerHTML = "";
+                                        var select = this.shadowRoot.getElementById("dimDropDownSel");
+                                        for (var i = 0; i < lt_valuesFatc.length; i++) {
+                                            var option = document.createElement("OPTION");
+                                            var lv_dimDesc = "";
+                                            var lv_memberId = lt_valuesFatc[i][dimension];
+                                            var lv_index = lt_values.findIndex(item => {
+                                                return item.ID == lv_memberId
+                                            });
+                                            if (lv_index >= 0) {
+                                                lv_dimDesc = lt_values[lv_index]
+                                            } else {
+                                                continue;
+                                            }
+                                            if (dimension == "Date") {
+                                                option.innerHTML = lt_values[lv_index].YEAR + " - " + lt_values[lv_index].MONTHDESC;
+                                                option.value = lt_values[lv_index].CALMONTH;
+                                            } else {
+                                                switch (displayOpt) {
+                                                case "Description":
+                                                    option.value = lt_values[lv_index].ID;
+                                                    option.innerHTML = lt_values[lv_index].Description;
+                                                    break;
+                                                case "ID":
+                                                    option.value = lt_values[lv_index].ID;
+                                                    option.innerHTML = lt_values[lv_index].ID;
+                                                case "ID - Description":
+                                                    option.value = lt_values[lv_index].ID;
+                                                    option.innerHTML = lt_values[lv_index].ID + " - " + lt_values[lv_index].Description;
+                                                    break;
+                                                case "Description - ID":
+                                                    option.value = lt_values[lv_index].ID;
+                                                    option.innerHTML = lt_values[lv_index].Description + " - " + lt_values[lv_index].ID;
+                                                    break;
+                                                }
+
+                                            }
+                                            select.options.add(option);
+                                        }
+                                    }
+                                }
+                            };
+                            xmlHttpFact.onerror = (e) => {
+                                console.error(xhr.statusText);
+                            };
+                            xmlHttpFact.send(null);
+
+                        }
+                    }
+
+                };
+                xmlHttpMaster.onerror = (e) => {
+                    console.error(xhr.statusText);
+                };
+                xmlHttpMaster.send(null);
+            }
+        }
+
         onCustomWidgetAfterUpdate(changedProperties) {
-            this.getData(); 
+            this.getData();
         }
         fireChanged() {
 
@@ -98,11 +193,11 @@
                 }));
 
         }
-        get selectedKey() { 
+        get selectedKey() {
             var e = this.shadowRoot.getElementById("dimDropDownSel");
             return e.options[e.selectedIndex].value;
         }
-        set selectedKey(_selectedKey) { 
+        set selectedKey(_selectedKey) {
             var e = this.shadowRoot.getElementById("dimDropDownSel");
             return e.options[e.selectedIndex].value = _selectedKey;
         }
