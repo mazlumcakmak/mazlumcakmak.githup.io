d0161682bdf910e8711fd94d6cfcc85be46e0cbf
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-15 16:28:06 +0300

    mazlum

--- calculate-bom/bill-of-material.js
+++ calculate-bom/bill-of-material.js
@@ -61,14 +61,14 @@
 
     async serviceRun() {
       debugger;
-      await (await this.$$.tblCost.getPlanning()).setUserInput({
+    /*  await (await this.$$.tblCost.getPlanning()).setUserInput({
         "@MeasureDimension": "[Measure].[parentId].&[amount]",
         "Date": "[Date].[YQM].&[202302]",
         "NTT_CW_COMPONENT":  "10000012",
         "NTT_CW_MATERIAL": "95000294",
         "Version": "public.2023V01"
       }, "9999900");
-      await (await this.$$.tblCost.getPlanning()).submitData();
+      await (await this.$$.tblCost.getPlanning()).submitData();*/
       
       // get token
       // var token = this.getToken();
