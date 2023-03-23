2a2570dc69463d6ec8fe946e6a35aec78f26bc35
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-23 20:10:39 +0300

    m

--- template-create/customWidgetCreate/template.js
+++ template-create/customWidgetCreate/template.js
@@ -1,6 +1,17 @@
 (function () { 
   debugger;
   let widgetId = "";
+  let jsonFile = {
+    "name" : "",
+    "description": "",
+    "newInstancePrefix": "",
+    "eula": "",
+    "vendor": "",
+    "license": "",
+    "id": "",
+    "version": "",
+    "icon": ""
+  };
   let tmpl = document.createElement("template");
   tmpl.innerHTML = `
         <style> 
@@ -44,11 +55,12 @@
       // cw
       var cwrequest = new XMLHttpRequest();
       var url =
-        "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWMaster?$filter=ID%20eq%20%27widgetId%27";
+        "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWMaster?$filter=ID%20eq%20%"+getFile()+"%27";
 
       cwrequest.open("GET", url, false);
       cwrequest.send(null);
       var lt_cw = JSON.parse(cwrequest.responseText).value;
+      console.log("cw",lt_cw);
 
       // cw
       var cwprequest = new XMLHttpRequest();
@@ -58,7 +70,14 @@
       cwprequest.open("GET", url, false);
       cwprequest.send(null);
       var lt_cwp = JSON.parse(cwprequest.responseText).value;
-      jsonText = "test json string";
+      jsonFile.name = lt_cw[0].ID;
+      jsonFile.description = lt_cw[0].Description;
+      jsonFile.newInstancePrefix = lt_cw[0].NEWINSTANCEPREFIX;
+      jsonFile.eula = lt_cw[0].EULA;
+      jsonFile.vendor = lt_cw[0].VENDOR;
+      jsonFile.license = lt_cw[0].LICENSE;
+      jsonFile.version = lt_cw[0].VERSION;
+      this.jsonText(jsonFile);
       
     }
 
@@ -95,11 +114,15 @@
     }
     set customWidgetName(_custId) {
       debugger;
+      this.getFile();
       widgetId = _custId;
     }
     get jsonText(){
       debugger;
       return jsonText;
+    }
+    set jsonText(_json){
+      jsonText = _json;
     }
   }
 
