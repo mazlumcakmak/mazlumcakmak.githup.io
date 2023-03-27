9239640a3d423fad4532d17ec354bf4f7d69315c
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-27 10:27:24 +0300

    mazlum

--- template-create/customWidgetCreate/template.js
+++ template-create/customWidgetCreate/template.js
@@ -136,7 +136,89 @@
       this.customWidgetName = '';
 
     }
+    connectedCallback() {
+      try {
+        if (window.commonApp) {
+          let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
 
+          if (outlineContainer && outlineContainer.getReactProps) {
+            let parseReactState = state => {
+              let components = {};
+
+              let globalState = state.globalState;
+              let instances = globalState.instances;
+              let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
+              let names = app.names;
+
+              for (let key in names) {
+                let name = names[key];
+
+                let obj = JSON.parse(key).pop();
+                let type = Object.keys(obj)[0];
+                let id = obj[type];
+
+                components[id] = {
+                  type: type,
+                  name: name
+                };
+              }
+
+              for (let componentId in components) {
+                let component = components[componentId];
+              }
+
+              let metadata = JSON.stringify({
+                components: components,
+                vars: app.globalVars
+              });
+
+              if (metadata != this.metadata) {
+                this.metadata = metadata;
+
+                this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                  detail: {
+                    properties: {
+                      metadata: metadata
+                    }
+                  }
+                }));
+              }
+            };
+
+            let subscribeReactStore = store => {
+              this._subscription = store.subscribe({
+                effect: state => {
+                  parseReactState(state);
+                  return {
+                    result: 1
+                  };
+                }
+              });
+            };
+
+            let props = outlineContainer.getReactProps();
+            if (props) {
+              subscribeReactStore(props.store);
+            } else {
+              let oldRenderReactComponent = outlineContainer.renderReactComponent;
+              outlineContainer.renderReactComponent = e => {
+                let props = outlineContainer.getReactProps();
+                subscribeReactStore(props.store);
+
+                oldRenderReactComponent.call(outlineContainer, e);
+              }
+            }
+          }
+        }
+      } catch (e) {}
+    }
+
+    disconnectedCallback() {
+      if (this._subscription) { // react store subscription
+        this._subscription();
+        this._subscription = null;
+      }
+    }
     async download(filename, text) {
 
 
@@ -327,7 +409,7 @@
       suggestedName: filename,
     };
     fileHandle = await window.showSaveFilePicker(opts);
-    const writable = await fileHandle.createWritable(); 
+    const writable = await fileHandle.createWritable();
     await writable.write(jsonBlob);
     await writable.close();
   }
