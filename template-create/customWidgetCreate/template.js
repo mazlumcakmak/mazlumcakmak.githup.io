0ba5d24c12e65d236dedfb77da9dc3694b2e59cf
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-27 11:43:17 +0300

    as

--- template-create/customWidgetCreate/template.js
+++ template-create/customWidgetCreate/template.js
@@ -52,7 +52,7 @@
       this._props.mainJs = "";
       this._props.builderJs = "";
       this.init();
-
+     
 
     }
 
@@ -61,109 +61,18 @@
         mode: "open",
       });
       shadowRoot.appendChild(tmpl.content.cloneNode(true));
-      this.shadowRoot.getElementById("main_file_type_select").addEventListener("change", this.getJsonFile.bind(this));
-
+      this.shadowRoot.getElementById("main_file_type_select").addEventListener("change", this.filtypeChange.bind(this));
+      
 
       //this.fireChanged();
 
     }
-    getJsonFile() {
-
-      widgetId = this._props.widgetId;
-      if (widgetId == "") return;
-
-      var cwrequest = new XMLHttpRequest();
-      var url =
-        "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWMaster?$filter=ID%20eq%20%27" + widgetId + "%27";
-
-      cwrequest.open("GET", url, false);
-      cwrequest.send(null);
-      if (cwrequest.status != 200) {
-
-        return;
-      }
-      var lt_cw = JSON.parse(cwrequest.responseText).value;
-
-      // cw
-      var cwprequest = new XMLHttpRequest();
-      var url =
-        "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWPMaster?$filter=CUSTOMWIDGET%20eq%20%27" + widgetId + "%27";
-
-      cwprequest.open("GET", url, false);
-      cwprequest.send(null);
-      if (cwprequest.status != 200) {
-        return;
-      }
-      var lt_cwp = JSON.parse(cwprequest.responseText).value;
-      jsonFile.name = lt_cw[0].ID;
-      jsonFile.description = lt_cw[0].Description;
-      jsonFile.newInstancePrefix = lt_cw[0].NEWINSTANCEPREFIX;
-      jsonFile.eula = lt_cw[0].EULA;
-      jsonFile.vendor = lt_cw[0].VENDOR;
-      jsonFile.license = lt_cw[0].LICENSE;
-      jsonFile.version = lt_cw[0].VERSION;
-      jsonFile.webcomponents[0].url = "enter url....";
-      jsonFile.webcomponents[0].tag = lt_cw[0].ID + "-main";
-
-      for (let i = 0; i < lt_cwp.length; i++) {
-        var methodName = lt_cwp[i].ORIGINALCWPID;
-        var memberId = lt_cwp[i].ORIGINALCWPID;
-        jsonFile.properties[memberId] = {
-          "description": lt_cwp[i].Description,
-          "type": lt_cwp[i].TYPE
-        }
-
-        // getter 
-        jsonFile.methods["get" + methodName] = {
-          "returnType": lt_cwp[i].TYPE,
-          "description": lt_cwp[i].Description,
-          "body": "return  this." + methodName + ";"
-        }
-
-        // setter
-        jsonFile.methods["set" + methodName] = {
-          "description": lt_cwp[i].Description,
-          "parameters": [{
-            "name": methodName,
-            "type": lt_cwp[i].TYPE,
-            "description": lt_cwp[i].Description
-          }],
-          "body": "this." + methodName + " = " + methodName + ";"
-        }
-
-      }
-      var fileName = lt_cw[0].ID;
-      var jsonFormater = JSON.stringify(jsonFile, null, 4);
-      this._firePropertiesChanged();
-      this.json = jsonFormater;
-      console.log(this.json);
-
-
+    
+    filtypeChange(){
+      var that = this;
+      getJsonFile(that);
     }
 
-    async downloadFile(filename, jsonText, maiText, builderText) {
-
-      var jsonBlob = new Blob([jsonText], {
-        type: 'application/json'
-      });
-      var mainJsBlob = new Blob([maiText], {
-        type: 'application/json'
-      });
-      let fileHandle;
-      const opts = {
-        types: [{
-          description: filename,
-          accept: {
-            'application/json': ['.json']
-          },
-        }],
-        suggestedName: filename,
-      };
-      fileHandle = await window.showSaveFilePicker(opts);
-      const writable = await fileHandle.createWritable();
-      await writable.write(jsonBlob);
-      await writable.close();
-    }
     connectedCallback() {
       console.log("callback func");
       try {
@@ -250,14 +159,14 @@
     }
     // after update
     onCustomWidgetAfterUpdate(changedProperties) {
-      this._firePropertiesChanged();
+     this. _firePropertiesChanged();
     }
 
     onCustomWidgetBeforeUpdate(changedProperties) {
 
     }
 
-    _firePropertiesChanged() {
+    _firePropertiesChanged() { 
       this.dispatchEvent(new CustomEvent("propertiesChanged", {
         detail: {
           properties: {
@@ -310,7 +219,103 @@
 
   customElements.define("template-create-main", templateCreate);
 
+  async function getJsonFile(that) {
 
+    widgetId = that._props.widgetId;
+    if (widgetId == "") return;
+
+    var cwrequest = new XMLHttpRequest();
+    var url =
+      "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWMaster?$filter=ID%20eq%20%27" + widgetId + "%27";
+
+    cwrequest.open("GET", url, false);
+    cwrequest.send(null);
+    if (cwrequest.status != 200) {
+
+      return;
+    }
+    var lt_cw = JSON.parse(cwrequest.responseText).value;
+
+    // cw
+    var cwprequest = new XMLHttpRequest();
+    var url =
+      "https://itelligencegroup-4.eu10.hcs.cloud.sap/api/v1/dataexport/providers/sac/C7mdde8dlqmog6pl6c85rpea2/CWPMaster?$filter=CUSTOMWIDGET%20eq%20%27" + widgetId + "%27";
+
+    cwprequest.open("GET", url, false);
+    cwprequest.send(null);
+    if (cwprequest.status != 200) {
+      return;
+    }
+    var lt_cwp = JSON.parse(cwprequest.responseText).value;
+    jsonFile.name = lt_cw[0].ID;
+    jsonFile.description = lt_cw[0].Description;
+    jsonFile.newInstancePrefix = lt_cw[0].NEWINSTANCEPREFIX;
+    jsonFile.eula = lt_cw[0].EULA;
+    jsonFile.vendor = lt_cw[0].VENDOR;
+    jsonFile.license = lt_cw[0].LICENSE;
+    jsonFile.version = lt_cw[0].VERSION;
+    jsonFile.webcomponents[0].url = "enter url....";
+    jsonFile.webcomponents[0].tag = lt_cw[0].ID + "-main";
+
+    for (let i = 0; i < lt_cwp.length; i++) {
+      var methodName = lt_cwp[i].ORIGINALCWPID;
+      var memberId = lt_cwp[i].ORIGINALCWPID;
+      jsonFile.properties[memberId] = {
+        "description": lt_cwp[i].Description,
+        "type": lt_cwp[i].TYPE
+      }
+
+      // getter 
+      jsonFile.methods["get" + methodName] = {
+        "returnType": lt_cwp[i].TYPE,
+        "description": lt_cwp[i].Description,
+        "body": "return  this." + methodName + ";"
+      }
+
+      // setter
+      jsonFile.methods["set" + methodName] = {
+        "description": lt_cwp[i].Description,
+        "parameters": [{
+          "name": methodName,
+          "type": lt_cwp[i].TYPE,
+          "description": lt_cwp[i].Description
+        }],
+        "body": "this." + methodName + " = " + methodName + ";"
+      }
+
+    }
+    var fileName = lt_cw[0].ID;
+    var jsonFormater = JSON.stringify(jsonFile, null, 4);
+    that._firePropertiesChanged();
+    that.json = jsonFormater;
+    console.log(that.json);
+
+
+  }
+
+  async function downloadFile(filename, jsonText, maiText, builderText) {
+
+    var jsonBlob = new Blob([jsonText], {
+      type: 'application/json'
+    });
+    var mainJsBlob = new Blob([maiText], {
+      type: 'application/json'
+    });
+    let fileHandle;
+    const opts = {
+      types: [{
+        description: filename,
+        accept: {
+          'application/json': ['.json']
+        },
+      }],
+      suggestedName: filename,
+    };
+    fileHandle = await window.showSaveFilePicker(opts);
+    const writable = await fileHandle.createWritable();
+    await writable.write(jsonBlob);
+    await writable.close();
+  }
 
 
 })();
