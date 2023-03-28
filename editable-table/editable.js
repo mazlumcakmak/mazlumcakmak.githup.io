d68dd021f2080358bb923448cfe1809b5c729dd0
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-28 16:03:26 +0300

    as

--- editable-table/editable.js
+++ editable-table/editable.js
@@ -94,7 +94,7 @@
         </script>        
     `;
 
-    class editTableWidget extends HTMLElement { 
+    class editTableWidget extends HTMLElement {
         constructor() {
             super();
 
@@ -257,7 +257,7 @@
 
             //### Controller ###
             sap.ui.define([
-                'sap/base/util/deepExtend', 
+                'sap/base/util/deepExtend',
                 'sap/ui/core/mvc/Controller',
                 'sap/ui/model/json/JSONModel',
                 'sap/m/ColumnListItem',
@@ -269,7 +269,54 @@
                 var TableController = Controller.extend("sap.m.sample.TableEditable.Table", {
 
                     onInit: function (evt) {
-                        this.oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+                        var lv_json = {
+                            "ProductCollection": [{
+                                    "ProductId": "HT-1000",
+                                    "Category": "Laptops",
+                                    "MainCategory": "Computer Systems",
+                                    "TaxTarifCode": "1",
+                                    "SupplierName": "Very Best Screens",
+                                    "WeightMeasure": 4.2,
+                                    "WeightUnit": "KG",
+                                    "Description": "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+                                    "Name": "Notebook Basic 15",
+                                    "DateOfSale": "2017-03-26",
+                                    "ProductPicUrl": "https://sdk.openui5.org/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+                                    "Status": "Available",
+                                    "Quantity": 10,
+                                    "UoM": "PC",
+                                    "CurrencyCode": "EUR",
+                                    "Price": 956,
+                                    "Width": 30,
+                                    "Depth": 18,
+                                    "Height": 3,
+                                    "DimUnit": "cm"
+                                },
+                                {
+                                    "ProductId": "HT-1001",
+                                    "Category": "Laptops",
+                                    "MainCategory": "Computer Systems",
+                                    "TaxTarifCode": "1",
+                                    "SupplierName": "Very Best Screens",
+                                    "WeightMeasure": 4.5,
+                                    "WeightUnit": "KG",
+                                    "Description": "Notebook Basic 17 with 2,80 GHz quad core, 17\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+                                    "Name": "Notebook Basic 17",
+                                    "DateOfSale": "2017-04-17",
+                                    "ProductPicUrl": "https://sdk.openui5.org/test-resources/sap/ui/documentation/sdk/images/HT-1001.jpg",
+                                    "Status": "Available",
+                                    "Quantity": 20,
+                                    "UoM": "PC",
+                                    "CurrencyCode": "EUR",
+                                    "Price": 1249,
+                                    "Width": 29,
+                                    "Depth": 17,
+                                    "Height": 3.1,
+                                    "DimUnit": "cm"
+                                }
+                            ]
+                        };
+                        this.oModel = new JSONModel(lv_json);
                         this.oTable = this.byId("idProductsTable");
                         this.getView().setModel(this.oModel);
                         this.oReadOnlyTemplate = this.byId("idProductsTable").removeItem(0);
@@ -351,7 +398,7 @@
             oView.placeAt(content);
 
 
-            
+
         });
     }
 
