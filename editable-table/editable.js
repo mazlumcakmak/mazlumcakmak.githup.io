5c11fdc810c5e2f342d98eee244b4c23026338c6
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-28 16:40:17 +0300

    as

--- editable-table/editable.js
+++ editable-table/editable.js
@@ -316,7 +316,7 @@
                                 }
                             ]
                         };
-                         var oModel = new JSONModel(lv_json);
+                        that.oModel = new JSONModel(lv_json);
                         that.oTable = that.byId("idProductsTable");
                         that.getView().setModel(that.oModel);
                         that.oReadOnlyTemplate = that.byId("idProductsTable").removeItem(0);
@@ -349,7 +349,54 @@
                     },
 
                     onEdit: function () {
-                        that.aProductCollection = deepExtend([], that.oModel.getProperty("/ProductCollection"));
+                        var lv_json = {
+                            "ProductCollection": [{
+                                    "ProductId": "HT-1000",
+                                    "Category": "Laptops",
+                                    "MainCategory": "Computer Systems",
+                                    "TaxTarifCode": "1",
+                                    "SupplierName": "Very Best Screens",
+                                    "WeightMeasure": 4.2,
+                                    "WeightUnit": "KG",
+                                    "Description": "Notebook Basic 15 with 2,80 GHz quad core, 15\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+                                    "Name": "Notebook Basic 15",
+                                    "DateOfSale": "2017-03-26",
+                                    "ProductPicUrl": "https://sdk.openui5.org/test-resources/sap/ui/documentation/sdk/images/HT-1000.jpg",
+                                    "Status": "Available",
+                                    "Quantity": 10,
+                                    "UoM": "PC",
+                                    "CurrencyCode": "EUR",
+                                    "Price": 956,
+                                    "Width": 30,
+                                    "Depth": 18,
+                                    "Height": 3,
+                                    "DimUnit": "cm"
+                                },
+                                {
+                                    "ProductId": "HT-1001",
+                                    "Category": "Laptops",
+                                    "MainCategory": "Computer Systems",
+                                    "TaxTarifCode": "1",
+                                    "SupplierName": "Very Best Screens",
+                                    "WeightMeasure": 4.5,
+                                    "WeightUnit": "KG",
+                                    "Description": "Notebook Basic 17 with 2,80 GHz quad core, 17\" LCD, 4 GB DDR3 RAM, 500 GB Hard Disc, Windows 8 Pro",
+                                    "Name": "Notebook Basic 17",
+                                    "DateOfSale": "2017-04-17",
+                                    "ProductPicUrl": "https://sdk.openui5.org/test-resources/sap/ui/documentation/sdk/images/HT-1001.jpg",
+                                    "Status": "Available",
+                                    "Quantity": 20,
+                                    "UoM": "PC",
+                                    "CurrencyCode": "EUR",
+                                    "Price": 1249,
+                                    "Width": 29,
+                                    "Depth": 17,
+                                    "Height": 3.1,
+                                    "DimUnit": "cm"
+                                }
+                            ]
+                        };
+                        that.aProductCollection = deepExtend([], lv_json);
                         that.byId("editButton").setVisible(false);
                         that.byId("saveButton").setVisible(true);
                         that.byId("cancelButton").setVisible(true);
