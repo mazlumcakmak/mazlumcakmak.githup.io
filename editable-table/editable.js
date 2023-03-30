41922308479a16fbd268d687a7e3f7e9e5f3e189
mazlumcakmak <mazlum.ckm@gmail.com>
2023-03-30 16:34:26 +0300

    as

--- editable-table/editable.js
+++ editable-table/editable.js
@@ -223,8 +223,7 @@
             //### Controller ###
             sap.ui.define([
                 'sap/ui/core/mvc/Controller',
-                'sap/ui/model/odata/v2/ODataModel',
-                'sap/ui/comp/sample/smarttable/mockserver/DemoMockServer'
+                'sap/ui/model/odata/v2/ODataModel', 
             ], function (Controller, ODataModel, DemoMockServer) {
                 "use strict";
 
