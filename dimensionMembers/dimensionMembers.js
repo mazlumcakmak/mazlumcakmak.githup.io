b1a83d569d52ea2eec2fe1f2eee36bb887b581b1
mazlumcakmak <mazlum.ckm@gmail.com>
2023-04-10 12:01:51 +0300

    create

--- dimensionMembers/dimensionMembers.js
+++ dimensionMembers/dimensionMembers.js
@@ -1,80 +1,89 @@
 (function () {
 
-        let tmpl = document.createElement("template");
-        tmpl.innerHTML = '';
-    
-        class className extends HTMLElement {
-            constructor() {
-                super();
-                this._props = {};
-                this.dimension = '';
-this.factData = '';
-this.filter = '';
+    let tmpl = document.createElement("template");
+    tmpl.innerHTML = '';
 
-                this.init();
-    
-            }
-    
-            init() {
-                let shadowRoot = this.attachShadow({
-                    mode: "open",
-                });
-                shadowRoot.appendChild(tmpl.content.cloneNode(true));
-    
-            }
-    
-            // after update
-            onCustomWidgetAfterUpdate(changedProperties) {
-                this._firePropertiesChanged();
-            }
-    
-            onCustomWidgetBeforeUpdate(changedProperties) {}
-    
-            _firePropertiesChanged() {
-                this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                        detail: {
-                            properties: {
-                              dimension : this.dimension,
-factData : this.factData,
-filter : this.filter,
+    class className extends HTMLElement {
+        constructor() {
+            super();
+            this._props = {};
+            this.dimension = '';
+            this.factData = '';
+            this.filter = '';
+            this.dimData = '';
 
-                            }
-                        }
-                    }));
-            }
-    
-            // getter setter
-             get dimension() {
- return this._props.dimension;
-}
- set dimension(value) {
- this._props.dimension = value;
-}
- get factData() {
- return this._props.factData;
-}
- set factData(value) {
- this._props.factData = value;
-}
- get filter() {
- return this._props.filter;
-}
- set filter(value) {
- this._props.filter = value;
-}
+            this.init();
 
-           
         }
-    
-        customElements.define("dimensionMembers-main", className);
-        async function masterDataService(tenant,provider,dimension) {
-var masterDataReq = new XMLHttpRequest();
-var url = "https://"+tenant+"/api/v1/dataexport/providers/sac/"+provider+"/"+dimension+"Master";masterDataReq.open("GET", url, false);
-masterDataReq.send(null);
-if (masterDataReq.status != 200) {
-  return;
- }
-return JSON.parse(masterDataReq.responseText).value
-}
-    
-    })();
+
+        init() {
+            let shadowRoot = this.attachShadow({
+                mode: "open",
+            });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
+        }
+
+        // after update
+        onCustomWidgetAfterUpdate(changedProperties) {
+            this._firePropertiesChanged();
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties) {}
+
+        _firePropertiesChanged() {
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        dimension: this.dimension,
+                        factData: this.factData,
+                        filter: this.filter,
+                        dimData: this.dimData,
+
+                    }
+                }
+            }));
+        }
+
+        // getter setter
+        get dimData() {
+            return this._props.dimData;
+        }
+        set dimData(value) {
+            this._props.dimData = value;
+        }
+        get dimension() {
+            return this._props.dimension;
+        }
+        set dimension(value) {
+            this._props.dimension = value;
+        }
+        get factData() {
+            return this._props.factData;
+        }
+        set factData(value) {
+            this._props.factData = value;
+        }
+        get filter() {
+            return this._props.filter;
+        }
+        set filter(value) {
+            this._props.filter = value;
+        }
+
+
+    }
+
+    customElements.define("dimensionMembers-main", className);
+    async function masterDataService(tenant, provider, dimension) {
+        var masterDataReq = new XMLHttpRequest();
+        var url = "https://" + tenant + "/api/v1/dataexport/providers/sac/" + provider + "/" + dimension + "Master";
+        masterDataReq.open("GET", url, false);
+        masterDataReq.send(null);
+        if (masterDataReq.status != 200) {
+            return;
+        }
+        return JSON.parse(masterDataReq.responseText).value
+    }
+
+})();
